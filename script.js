687ec0da87ef46caff6a6ffbe7a87884f23226e0
cawl1010 <dieterhillje@gmail.com>
2022-05-17 15:46:34 -0400

    Added Weather API to DOM

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 "use strict";
 
+const weather = document.querySelector("#weather");
+const button = document.querySelector("#weather-button");
+
+const updateWeather = async () => {
+  const weatherText = await getWeather();
+  console.log(weatherText);
+  const newLI = document.createElement("LI");
+  newLI.append(weatherText);
+  weather.append(newLI);
+};
+
 async function getWeather() {
   try {
+    const config = { headers: { Accept: "application/json" } };
     const response = await axios.get(
-      "http://dataservice.accuweather.com/currentconditions/v1/327351?apikey=8arva3wAj3hpbjOnGIFqI0av5aD0UYxt"
+      "http://dataservice.accuweather.com/currentconditions/v1/327351?apikey=8arva3wAj3hpbjOnGIFqI0av5aD0UYxt",
+      config
     );
-    console.log(response);
+    // console.log(response.data[0].WeatherText);
+    return response.data[0].WeatherText;
   } catch (error) {
     console.error(error);
   }
 }
 
-getWeather();
+button.addEventListener("click", updateWeather);
+
+// getWeather();
