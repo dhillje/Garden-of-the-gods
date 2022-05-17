83fba8942dbd99a561603d38057ae439e369363a
cawl1010 <dieterhillje@gmail.com>
2022-05-17 15:07:07 -0400

    Added Weather API into script

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+async function getWeather() {
+  try {
+    const response = await axios.get(
+      "http://dataservice.accuweather.com/currentconditions/v1/327351?apikey=8arva3wAj3hpbjOnGIFqI0av5aD0UYxt"
+    );
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getWeather();
